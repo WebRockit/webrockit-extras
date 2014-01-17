7e9d2cabb1a3c345fd32282768875a013b3cc95e
Mike <xyntrix@gmail.com>
2014-01-17 14:34:19 -0700

    o Initial Public Release

---
+++ README.md
@@ -0,0 +1,8 @@
+### webrockit-extra
+
+This is a collection of skeleton configs and configuration scripts to
+manage webrockit related resources.
+
+### License
+   webrockit-extras is released under the MIT license, and may bundle other liberally licensed OSS components [License](LICENSE.txt)  
+   [Third Party Software](third-party.txt)
---
+++ poller/install-poller.sh
@@ -0,0 +1,27 @@
+#1/bin/sh
+
+### This has been tested with CentOS 6.x x86_64 
+###  
+# Install some base requirements
+yum -y install wget freetype fontconfig 
+
+# Install WebRockit repo
+wget https://bintray.com/webrockit/webrockit/rpm -O /etc/yum.repos.d/bintray-webrockit-webrockit.repo --no-check-certificate 
+
+# Install webRockit poller packages
+yum -y install webrockit-poller webrockit-phantomas webrockit-nodejs-bin
+
+# Install ruby 2.0 with rvm
+curl -L get.rvm.io | bash -s stable
+source /etc/profile.d/rvm.sh
+rvm requirements
+rvm install 2.0
+rvm use 2.0 --default
+rvm rubygems current
+
+# Install gem: ghost
+gem install ghost
+
+# test poller:
+
+/opt/phantomjs/collectoids/webrockit-poller/webrockit-poller.rb --url http://github.com
