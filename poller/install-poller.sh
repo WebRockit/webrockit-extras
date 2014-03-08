c3110abf87ccbc51406e1fd8e719585e2f9e0566
Mike <xyntrix@gmail.com>
2014-03-07 17:27:01 -0700

    Update install-poller.sh
    o install can (and should) be installed via non-root user because of rvm insanity with environment detection

--- poller/install-poller.sh
+++ poller/install-poller.sh
@@ -6,34 +6,34 @@
 ###    curl -L http://x.co/wrclientc6  | bash -s stable
 
 # Install some base requirements
-yum -y install wget freetype fontconfig 
+sudo yum -y install wget freetype fontconfig 
 
 # Install WebRockit repo
-wget https://bintray.com/webrockit/webrockit/rpm -O /etc/yum.repos.d/bintray-webrockit-webrockit.repo --no-check-certificate 
+sudo wget https://bintray.com/webrockit/webrockit/rpm -O /etc/yum.repos.d/bintray-webrockit-webrockit.repo --no-check-certificate 
 
 # Install webRockit poller packages
-yum -y install webrockit-poller webrockit-phantomas webrockit-nodejs-bin
+sudo yum -y install webrockit-poller webrockit-phantomas webrockit-nodejs-bin
 
 # Install ruby 2.0 with rvm
-curl -L get.rvm.io | bash -s stable
+curl -L get.rvm.io | sudo bash -s stable
 source /etc/profile.d/rvm.sh
-rvm requirements
-rvm install 2.0.0-p451
-rvm use 2.0.0-p451 --default
-rvm rubygems current
+sudo rvm requirements
+sudo rvm install 2.0.0-p451
+sudo rvm use 2.0.0-p451 --default
+sudo rvm rubygems current
 
 # Install gem: ghost
-gem install ghost
+sudo gem install ghost
 
 # adjust sensu to match number of cores available x2, with a minimum of 4
 CORES=`grep processor /proc/cpuinfo | tail -n1 | awk '{print $NF+1}'`
 if [ ${CORES} -lt 4 ]; then CORES=4; fi
 if [ -e "/opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb" ]; then
-  sed -i -c -e 's/EventMachine.threadpool_size\ =\ 20/EventMachine.threadpool_size\ =\ '${CORES}'/g' /opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb
+  sudo sed -i -c -e 's/EventMachine.threadpool_size\ =\ 20/EventMachine.threadpool_size\ =\ '${CORES}'/g' /opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb
 fi
 
 # why aren't you using sudoers.d?
-egrep -q '^#includedir /etc/sudoers.d$' /etc/sudoers || echo '#includedir /etc/sudoers.d' >> /etc/sudoers
+sudo egrep -q '^#includedir /etc/sudoers.d$' /etc/sudoers || echo '#includedir /etc/sudoers.d' >> /etc/sudoers
 
 # test poller:
 
