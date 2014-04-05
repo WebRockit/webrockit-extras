07cbe3a1516a814ce5fd7f4fd8a044538e514781
Mike <xyntrix@gmail.com>
2014-04-05 08:15:26 -0700

    Update install-poller.sh
    o changes for cent5 made cent6 install wonky, reverting back to root install. will fix this by removing rvm dependency in the future.

--- poller/install-poller.sh
+++ poller/install-poller.sh
@@ -14,26 +14,28 @@ sudo wget http://yum.webrockit.io/repos/webrockit.repo -O /etc/yum.repos.d/webro
 # Install webRockit poller packages
 sudo yum -y install webrockit-poller webrockit-phantomas webrockit-nodejs-bin
 
-# Install ruby 2.0 with rvm
-curl -L get.rvm.io | sudo bash -s stable
+# TODO: convert this to use Sensu omnibus ruby
+# Install ruby 2.0 with rvm, as root under CentOS 5
+sudo su -
+curl -L get.rvm.io |  bash -s stable
 source /etc/profile.d/rvm.sh
-sudo rvm requirements
-sudo rvm install 2.0.0-p451
+rvm requirements
+rvm install 2.0.0-p451
 rvm use 2.0.0-p451 --default
 rvm rubygems current
 
 # Install gem: ghost
-sudo gem install ghost
+gem install ghost
 
 # adjust sensu to match number of cores available x2, with a minimum of 4
 CORES=`grep processor /proc/cpuinfo | tail -n1 | awk '{print $NF+1}'`
 if [ ${CORES} -lt 4 ]; then CORES=4; fi
 if [ -e "/opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb" ]; then
-  sudo sed -i -c -e 's/EventMachine.threadpool_size\ =\ 20/EventMachine.threadpool_size\ =\ '${CORES}'/g' /opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb
+  sed -i -c -e 's/EventMachine.threadpool_size\ =\ 20/EventMachine.threadpool_size\ =\ '${CORES}'/g' /opt/sensu/embedded/lib/ruby/gems/2.0.0/gems/eventmachine-1.0.3/lib/eventmachine.rb
 fi
 
 # why aren't you using sudoers.d?
-sudo bash -c "egrep -q '^#includedir /etc/sudoers.d$' /etc/sudoers || sudo -- echo '#includedir /etc/sudoers.d' >> /etc/sudoers"
+bash -c "egrep -q '^#includedir /etc/sudoers.d$' /etc/sudoers || sudo -- echo '#includedir /etc/sudoers.d' >> /etc/sudoers"
 
 # test poller:
 
