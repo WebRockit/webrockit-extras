9261171df1d725112ff57de7c20b979c0929d05a
Mike M <xyntrix@gmail.com>
2014-04-25 01:53:04 -0700

    o new ghost gem install installs to sensu omnibus path instead of via rvm

--- poller/install-poller.sh
+++ poller/install-poller.sh
@@ -5,6 +5,28 @@
 ### Automate this by running:
 ###    curl -L http://x.co/wrclientc6  | bash -s stable
 
+# We need the Sensu client to be installed. Let's check for it.
+if [! -e /opt/sensu/embedded/bin/gem ]; then
+   echo "Please install the Sensu client."
+   echo "See:  http://sensuapp.org/docs/0.12/guide"
+   echo ""
+   echo "CentOS Quick Client Install:"
+   echo "########################"
+   echo "echo '[sensu]"
+   echo "name=sensu-main"
+   echo "baseurl=http://repos.sensuapp.org/yum/el/\$releasever/\$basearch/"
+   echo "gpgcheck=0"
+   echo "enabled=1' > /etc/yum.repos.d/sensu.repo"
+   echo "yum -y install sensu"
+   exit
+fi
+
+export OLD_GEM_PATH=${GEM_PATH}
+export GEM_PATH=/opt/sensu/embedded/lib/ruby/gems/2.0.0:${GEM_PATH}
+# Install gem: ghost, into Sensu path
+/opt/sensu/embedded/bin/gem install ghost
+export GEM_PATH=${OLD_GEM_PATH}
+
 # Install some base requirements
 yum -y install wget freetype fontconfig
 
@@ -14,17 +36,6 @@ wget http://yum.webrockit.io/repos/webrockit.repo -O /etc/yum.repos.d/webrockit.
 # Install webRockit poller packages
 yum -y install webrockit-poller webrockit-phantomas webrockit-nodejs-bin
 
-# TODO: convert this to use Sensu omnibus ruby
-# Install ruby 2.0 with rvm, as root under CentOS 5
-curl -L get.rvm.io |  bash -s stable
-source /etc/profile.d/rvm.sh
-rvm requirements
-rvm install 2.0.0-p451
-rvm use 2.0.0-p451 --default
-rvm rubygems current
-
-# Install gem: ghost
-gem install ghost
 
 # adjust sensu to match number of cores available x2, with a minimum of 4
 CORES=`grep processor /proc/cpuinfo | tail -n1 | awk '{print $NF+1}'`
@@ -35,9 +46,6 @@ fi
 
 # why aren't you using sudoers.d?
 bash -c "egrep -q '^#includedir /etc/sudoers.d$' /etc/sudoers || sudo -- echo '#includedir /etc/sudoers.d' >> /etc/sudoers"
-
-# TODO: move this into poller rpm
-egrep -q GEM_HOME /etc/sudoers.d/sensu || echo 'Defaults:sensu env_keep=GEM_HOME' >> /etc/sudoers.d/sensu
 
 # test poller:
 
